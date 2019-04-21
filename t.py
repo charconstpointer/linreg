77b92c4ca21ae1c718b6d2378bf2ff32a48699aa
const <attwistingnether@gmail.com>
2019-04-21 11:44:56 +0200

    .

--- t.py
+++ t.py
@@ -8,14 +8,17 @@ y = []
 
 for _ in range(100000):
     X.append([_])
-    y.append([math.log10(_+1)])
+
+    y.append([(_*2)])
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 reg = LinearRegression().fit(X_train, y_train)
 print(reg.score(X_train, y_train))
+
 plt.scatter(X_test, y_test, color='green')
 plt.scatter(X, y, color='red')
 plt.plot(X_train, reg.predict(X_train), color='blue')
+
 print(reg.predict([[4]]))
 plt.show()
